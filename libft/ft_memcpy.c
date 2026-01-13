d58e6b5ff3a3d80304195170d8a157b7344626a5
Jeanpool Zambrano <jezambra@c1r5s4.42madrid.com>
2026-01-13 18:38:43 +0100

    ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jezambra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 12:46:23 by jezambra          #+#    #+#             */
-/*   Updated: 2026/01/13 12:48:23 by jezambra         ###   ########.fr       */
+/*   Updated: 2026/01/13 18:36:27 by jezambra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,30 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char	
-	char	*ptr;
-	size_t i;
+	char	*des;
+	char	*sr;
+	size_t	i;
 
-	ptr = (char *)dest;
+	des = (char *)dest;
+	sr = (char *)src;
+	
 	i = 0;
+	if (sr[i] == '\0')
+		return (des);
+	while (sr[i] && i < n)
+	{
+		des[i] = sr[i];
+		i++;
+	}
+	return (des);
+}
+int	main(void)
+{
+	char	dest[] = "ca va";
+	char	src[] = "bien";
+
+	ft_memcpy(dest, src, 4);
+	printf("%s\n", (char *)ft_memcpy(dest, src, 5));
+	printf("%s\n", (char *)memcpy(dest, src, 5));
+	return (0);
 }
