3030bafd7af108e8eafa3dd330a057154df4f8d6
Jeanpool <patrickjeam2@gmail.com>
2026-01-21 23:19:31 +0100

    checking tomorrow

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jezambra <jezambra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 16:52:52 by jezambra          #+#    #+#             */
-/*   Updated: 2026/01/21 19:15:52 by jezambra         ###   ########.fr       */
+/*   Updated: 2026/01/21 23:18:09 by jezambra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 
 char	**ft_split(char const *s, char c)
 {
-/*esta funcion lo que hace es dividir toda una cadena de string y hacer 
-un array para cada palabra del string, ejemplo hola que tal, tiene que hacer un
-array con el carac ter delimitador que, ser el char c, digamos que el cacracter
-es , o  espaio, pues cuanndo encusntre el espacio o la co,a lo va adividi
-en un nuevo array */
 	char	**str;
 	int	word;
 	int	i;
@@ -38,17 +33,16 @@ en un nuevo array */
 		else 
 			i++;
 	}
-/*aqui generamos bucles anidados y condiciomes, en el primer while decimos que 
-si la palabra existe va a esstratr en la primera condicion del if, y decimos que 
-si la posicion i de la extring existe y es diferenrte al delimitador va a sumar la
-palabra, el otro while es para que avance letra por letra para comprovar si es o
-no  un delimitador, si es diferente avanza la posicion de i osea de la string
-si no lo es se sabe y va a sumar la primera palabra del primer while */
 	str = malloc(sizeof(char const *) * (word + 1));
 	if(!str)
 		return (NULL);
-/*aqui generamos un malloc de cada palabra mas 1 para el nulo y lo blindamos 
-pos si la memoria falla */
+	i = 0;
 	j = 0;
-	
+	while (s[i])
+	{
+		if (s[i] != c)
+//sera que aqui toca hacer lo mismo de arriba y en el bucle cuando llegue 
+//al delimitador generar un malloc ? preguntar manana esto, la insertidumbre 
+//me consume.
+	}
 }
