72bd506fd16f73181c3a6b08270bb70f58de887f
Jeanpool <patrickjeam2@gmail.com>
2026-01-23 00:05:44 +0100

    split optimisado pero sujeto a revision

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jezambra <jezambra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 16:52:52 by jezambra          #+#    #+#             */
-/*   Updated: 2026/01/22 19:40:06 by jezambra         ###   ########.fr       */
+/*   Updated: 2026/01/23 00:03:51 by jezambra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,118 @@
 #include <string.h>
 #include <stdio.h>
 
+static int	count_words(const char *s, char c)
+{
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			word++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (word);
+}
+
+static char	**words_save_memory(char const *s, char c, int word)
+{
+	char	**str;
+	int		i;
+	int		j;
+	int		start;
+
+	i = 0;
+	j = 0;
+	str = malloc(sizeof(char const *) * (word + 1));
+	if (!str)
+		return (NULL);
+	str[j] = NULL;
+	while (j < word)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		str[j] = malloc(i - start + 1);
+		if (!str[j])
+		{
+			while (str[j])
+			{
+				free(str[j]);
+				j--;
+			free(str);
+			return (NULL);
+			}
+		}
+		j++;
+	}
+	return (str);
+}
+
+static void	words_in_box(char **str, char const *s, char c, int word)
+{
+	int	i;
+	int	j;
+	int	i2;
+
+	i = 0;
+	j = 0;
+	while (j < word)
+	{
+		i2 = 0;
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			str[j][i2] = s[i];
+			i2++;
+			i++;
+		}
+		str[j][i2] = '\0';
+		j++;
+	}
+}
+
 char	**ft_split(char const *s, char c)
+{
+	char	**str;
+	int		word;
+
+	if (!s)
+		return (NULL);
+	word = count_words(s, c);
+	str = words_save_memory(s, c, word);
+	if (!str)
+		return (NULL);
+	words_in_box(str, s, c, word);
+	return (str);
+}
+int main(void)
+{
+	char **result;
+	int i;
+
+	result = ft_split("hola, mundo", ',');
+	i = 0;
+	while (result[i])
+	{
+		printf("Palabra %d: \"%s\"\n", i, result[i]);
+		free(result[i]); // Liberar malloc de cada palabra
+		i++;
+	}
+	free(result); // Liberar gran malloc
+	return (0);
+}
+/*char	**ft_split(char const *s, char c)
 {
 	char	**str;
 	int	word;
@@ -99,4 +210,4 @@ int main(void)
 	}
 	free(result); // Liberar gran malloc
 	return (0);
-}
+}*/
